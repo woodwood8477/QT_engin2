b48a1fde58a2e9afa71e5bc1d9da88a188c8b88f
TsubasaIkeda <ikeda_tsubasa@quantrios.com>
2026-05-07 00:30:57 +0900

    feat: METAFONT実験へのフローティングリンクを追加

---
+++ src/components/MetafontLink.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const HREF = './experiments/quantrios-metafont/';
+</script>
+
+<template>
+  <a
+    class="metafont-link"
+    :href="HREF"
+    aria-label="Open QuanTRIOS METAFONT experiment"
+  >
+    METAFONT
+  </a>
+</template>
