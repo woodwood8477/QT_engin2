c0958721f427d275c52d88e456b3e7df884a0102
TsubasaIkeda <ikeda_tsubasa@quantrios.com>
2026-05-07 00:23:48 +0900

    chore: Vite + Vue 3 + TypeScriptプロジェクトを初期化

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  base: './',
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    target: 'es2020',
+    sourcemap: true
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true
+  }
+});
